b0cc88e38288e0167d893a7406658590f7a55e09
Fabrizio Rondinella <94895169+JackER4565@users.noreply.github.com>
2023-12-04 18:04:59 -0300

    asd

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // get mails, subject and body from params and create a new mail vanilla javascript
 
 function test() {
+  alert('test');
 //get params
 const params = new URLSearchParams(window.location.search);
 const mail = params.get('mail');
